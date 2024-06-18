54c50b801e0f89c2fa236b94aed07f69c03d35b4
HedvigS <hedvig.skirgard@gmail.com>
2024-06-18 14:45:11 +0200

    Update drop_duplicate_tips_random.R

--- functions/drop_duplicate_tips_random.R
+++ functions/drop_duplicate_tips_random.R
@@ -2,8 +2,8 @@
 #'
 #' @param tree 	an object of class "phylo" with glottocodes in tip labels.
 #' @param merge_dialects a logical specifying whether to replace dialect tip labels with the glottocode of the language that is their parent, and then drop all but one
-#' @param LanguageTable data-frame of a cldf LanguageTable with columns for glottocodes per languoid and dialects language parent glottocode. If merge_dialects is set to TRUE, a LanguageTable is necessary.
-#' @param trim_tip_label_to_first_eight a logical specifying whether we should trim the tip label to the first 8 characters. If the tip labels contain more than just glottocodes, it is necessary to trim away what is not glottocodes. If set to TRUE, only the first 8 characters are retained. This is necessary for the EDGE-trees as they contain names as well (e.g. "bamu1257_Bamu_Kiwaian"). If the tip labels only consist of glottocodes, this parameter can be set to TRUE or FALSE without difference.
+#' @param LanguageTable data-frame of a cldf LanguageTable with columns Glottocode and Language_level_ID. If merge_dialects is set to TRUE, a LanguageTable is necessary.
+#' @param trim_tip_label_to_first_eight a logical specifying whether we should first trim the tip labels to the first 8 characters. If the tip labels contain more than just glottocodes, it is necessary to trim away the rest. If set to TRUE, only the first 8 characters are retained. This is convenient for the global tree from Bouckaert et al (2023) where the tip labels are of the style "cent2004_CentralBai_Sino-Tibetan". If the tip labels already only consist of glottocodes, this parameter can be set to TRUE or FALSE without difference.
 #' @return tree without duplicates or multiple dialects for the same language.
 #' @author Hedvig Skirgård
 #' @export
@@ -20,46 +20,33 @@ if(trim_tip_label_to_first_eight == TRUE){
     tree$tip.label <- tree$tip.label %>% substr(1, 8)
 }
 
-
 if(merge_dialects == TRUE){
 
 if(is.null(LanguageTable)){
     stop("LanguageTable is NULL. LanguageTable is necessary for merging dialects.\n")
 }else{
 
-if(!("Glottocode" %in% colnames(LanguageTable)  & "Language_ID" %in% colnames(LanguageTable) | "Language_level_ID" %in% colnames(LanguageTable))){
-stop("LanguageTable does not contain all the necessary columns: ´Glottocode' and 'Language_ID' or 'Language_level_ID'.")
+if(!("Glottocode" %in% colnames(LanguageTable)  &  "Language_level_ID" %in% colnames(LanguageTable))){
+stop("LanguageTable does not contain all the necessary columns: ´Glottocode' and 'Language_level_ID'.")
     }
-
-# The language level ID column is named different in different, CLDF datasets.
-# setting them to the same
-if("Language_ID" %in% colnames(LanguageTable)){
-    LanguageTable   <- LanguageTable %>%
-        dplyr::select(Language_ID = Glottocode, Language_level_ID = Language_ID)
-}
-
-if("Language_level_ID" %in% colnames(LanguageTable) & "ID" %in% colnames(LanguageTable)){
-    LanguageTable   <- LanguageTable %>%
-        dplyr::select(Language_ID = Glottocode, Language_level_ID)
-}
 
 #some LanguageTables only contain values for Language_level_ID if the languoid is a dialect. Here we insert the language level glottocode if the level is language or family as well.
 LanguageTable <-    LanguageTable %>%
         dplyr::mutate(Language_level_ID = ifelse(is.na(Language_level_ID) |
-                                                     Language_level_ID == "", Language_ID, Language_level_ID))
+                                                     Language_level_ID == "", Glottocode, Language_level_ID))
 
 #rename tip labels to the glottocode of the language level if tip is dialect
 tip_labels_df <- tree$tip.label %>%
     as.data.frame() %>%
-    dplyr::rename(Language_ID = ".") %>%
-    dplyr::left_join(LanguageTable, by = "Language_ID")
+    dplyr::rename(Glottocode = ".") %>%
+    dplyr::left_join(LanguageTable, by = "Glottocode")
 
 tree$tip.label <- tip_labels_df$Language_level_ID
 
 }}
 
 
-#keeping just one tip per language in the entire tree. Anytime where there are duplicate tip labels, only one tip is kept. Selection is random.
+#keeping just one tip per unique glottocode tip label in the entire tree. Anytime where there are duplicate tip labels, only one tip is kept. Selection is random.
 to_keep <- tree$tip.label %>%
               as.data.frame() %>%
     dplyr::rename(tip.label = ".") %>%
