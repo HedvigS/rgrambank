6ba22b381cc3eb594531c7d8ed9d5e6e8fb037bc
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2024-06-20 16:24:58 +0200

    Update example_make_theo_scores.R

--- example_scripts/example_make_theo_scores.R
+++ example_scripts/example_make_theo_scores.R
@@ -8,13 +8,13 @@ library(tidyverse)
 #install_github("SimonGreenhill/rcldf", dependencies = TRUE, ref = "v1.2.0")
 library(rcldf)
 
-#GB_rcldf_obj <- rcldf::cldf("../../../../Grambank 2.0/grambank/cldf/StructureDataset-metadata.json", load_bib = F)
+GB_rcldf_obj_v2 <- rcldf::cldf("../../../../Grambank 2.0/grambank/cldf/StructureDataset-metadata.json", load_bib = F)
 # fetching Grambank v1.0.3 from Zenodo using rcldf (requires internet)
-GB_rcldf_obj <- rcldf::cldf("https://zenodo.org/record/7844558/files/grambank/grambank-v1.0.3.zip", load_bib = F)
+GB_rcldf_obj_v1 <- rcldf::cldf("https://zenodo.org/record/7844558/files/grambank/grambank-v1.0.3.zip", load_bib = F)
 
-LanguageTable <- GB_rcldf_obj$tables$LanguageTable
-ValueTable <- GB_rcldf_obj$tables$ValueTable
-ParameterTable <- GB_rcldf_obj$tables$ParameterTable
+LanguageTable <- GB_rcldf_obj_v2$tables$LanguageTable
+ValueTable <- GB_rcldf_obj_v2$tables$ValueTable
+ParameterTable <- GB_rcldf_obj_v1$tables$ParameterTable
 
 source("../functions/make_binary_ParameterTable.R")
 source("../functions/make_binary_ValueTable.R")
@@ -22,7 +22,7 @@ source("../functions/reduce_ValueTable_to_unique_glottocodes.R")
 source("../functions/make_theo_scores.R")
 
 ValueTable_binary <- make_binary_ValueTable(ValueTable = ValueTable, keep_multistate = FALSE, keep_raw_binary = TRUE)
-ParameterTable_binary <- make_binary_ParameterTable(ParameterTable = ParameterTable, keep_multi_state_features = FALSE)
+ParameterTable_binary <- make_binary_ParameterTable(ParameterTable = ParameterTable, keep_multi_state_features = FALSE, keep_raw_binary = TRUE)
 
 ValueTable_binary_reduced <- reduce_ValueTable_to_unique_glottocodes(ValueTable = ValueTable_binary, LanguageTable = LanguageTable, merge_dialects = TRUE, method = "combine_random") %>% 
   dplyr::select(-Language_ID) %>% 
