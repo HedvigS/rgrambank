b24cc81a4a2e0c70ec49e9e416caac94a1550458
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2024-06-24 16:30:53 +0200

    Update drop_duplicate_glottocode_tips_random.R

--- functions/drop_duplicate_glottocode_tips_random.R
+++ functions/drop_duplicate_glottocode_tips_random.R
@@ -1,10 +1,11 @@
 #' If a language tree has tips with duplicate tip labels, drop all but one at random. If there are tips which are dialects of the same language, you can choose to drop all but one.
 #'
-#' @param tree 	an object of class "phylo" with glottocodes in tip labels.
+#' @param tree 	an object of class "phylo". Tip-labels need to be unique within the tree, but can represent duplicates of Glottocodes.
 #' @param merge_dialects a logical specifying whether to replace dialect tip labels with the glottocode of the language that is their parent, and then drop all but one
-#' @param LanguageTable data-frame of a cldf LanguageTable with columns Glottocode and Language_level_ID. If merge_dialects is set to TRUE, a LanguageTable is necessary.
-#' @param trim_tip_label_to_first_eight a logical specifying whether we should first trim the tip labels to the first 8 characters. If the tip labels contain more than just glottocodes, it is necessary to trim away the rest. If set to TRUE, only the first 8 characters are retained. This is convenient for the global tree from Bouckaert et al (2023) where the tip labels are of the style "cent2004_CentralBai_Sino-Tibetan". If the tip labels already only consist of glottocodes, this parameter can be set to TRUE or FALSE without difference.
-#' @return tree without duplicates or multiple dialects for the same language.
+#' @param LanguageTable data-frame of tip-labels matched to Glottocodes.
+#' @param LanguageTable2 data-frame of Glottocodes matched to Language_level_ID. If merge-dialects is TRUE and LanguageTable lacks the column 'Language_level_ID', LanguageTable2 needs to be specified.
+#' @param trim_tip_label_to_first_eight a logical specifying whether we should first trim the tip labels to the first 8 characters in the output tree. If the tip labels contain more than just glottocodes, it is necessary to trim away the rest. If set to TRUE, only the first 8 characters are retained. This is convenient for the global tree from Bouckaert et al (2023) where the tip labels are of the style "cent2004_CentralBai_Sino-Tibetan". If the tip labels already only consist of glottocodes, this parameter can be set to TRUE or FALSE without a difference.
+#' @return tree without tips with duplicate Glottocodes, optionally also merged dialects.
 #' @author Hedvig Skirgård
 #' @export
 
