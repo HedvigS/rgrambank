0c1e233f6f469fbb81e661d3cdacb5dca9c3abc8
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2024-07-18 15:33:46 +0200

    Update match_to_rgb.R

--- functions/match_to_rgb.R
+++ functions/match_to_rgb.R
@@ -3,6 +3,7 @@
 #' @param x data-frame or matrix. Identifiers should be rownames and the three relevant columns for mapping to RGB should be numeric
 #' @param first_three logical. If TRUE then the first three columns are used to map to RGB
 #' @param cols = character vector. If first_three is FALSE, provide the names of the three relevant columns here
+#' @author Hedvig Skirgård and Damián Blasi
 #' @export
 
 match_to_RGB <- function(x = NULL, 
