1a98019ce8b4e3149dff843cd86a2e885838606a
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2024-07-24 18:29:30 +0200

    Update combine_Glottolog_ValueTable_LanguageTable.R

--- functions/combine_Glottolog_ValueTable_LanguageTable.R
+++ functions/combine_Glottolog_ValueTable_LanguageTable.R
@@ -1,9 +1,10 @@
-#' Adds info about isolates to tables of languages with glottocodes.
+#' Combines Glottolog-cldf ValueTable and LanguageTable, in an opinionated manner.
 #'
 #' @param Glottolog_LanguageTable data.frame
 #' @param Glottolog_ValueTable data.frame
 #' @return Data-frame with information from Glottolog's LanguageTable and ValueTabled joined
-#' @note The information in "Comment" and "Source" from the ValueTable is not retained. The column called "Language_ID" in LanguageTable is renamed to "Language_level_ID" to reduce confusion with foreign keys in other tables.
+#' @note The information in "Comment" and "Source" from the ValueTable is not retained in the output data-frame. The column called "Language_ID" in LanguageTable is renamed to "Language_level_ID" to reduce confusion with foreign keys in other tables. The column ID in LanguageTable is renamed "Language_ID".
+#' @return A data-frame that combines information from glottolog-cldf ValueTable and LanguageTable. See note for details on modifications.
 #' @author Hedvig Skirgård
 #' @export
 
