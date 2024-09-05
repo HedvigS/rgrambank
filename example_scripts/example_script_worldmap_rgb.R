aee3b9bb3f610b75174a2687a55d5acf7ee5f880
HedvigS <hedvig.skirgard@gmail.com>
2024-09-05 23:02:55 +0200

    Update example_script_worldmap_rgb.R

--- example_scripts/example_script_worldmap_rgb.R
+++ example_scripts/example_script_worldmap_rgb.R
@@ -22,7 +22,7 @@ library(rcldf)
 library(rgrambank)
 
 if(!dir.exists("ouptut")){dir.create("output")}
-if(!dir.exists("ouptut/plots")){dir.create("output/plots")}
+if(!dir.exists("output/plots")){dir.create("output/plots")}
 
 # fetching Grambank v1.0.3 from Zenodo using rcldf (requires internet)
 GB_rcldf_obj <- rcldf::cldf("https://zenodo.org/record/7844558/files/grambank/grambank-v1.0.3.zip", load_bib = F)
@@ -30,32 +30,39 @@ GB_rcldf_obj <- rcldf::cldf("https://zenodo.org/record/7844558/files/grambank/gr
 ValueTable <- GB_rcldf_obj$tables$ValueTable
 LanguageTable <- GB_rcldf_obj$tables$LanguageTable
 
-#make Grambank ValueTable binary
-ValueTable_binary <- rgrambank::make_binary_ValueTable(ValueTable = ValueTable, keep_multistate = F, keep_raw_binary = T)
-
 #remove duplicate glottocodes and merge dialects
-ValueTable_dialect_reduced <- rgrambank::reduce_ValueTable_to_unique_glottocodes(ValueTable = ValueTable_binary,
+ValueTable_dialect_reduced <- rgrambank::reduce_ValueTable_to_unique_glottocodes(ValueTable = ValueTable,
                                                                       LanguageTable = LanguageTable,
                                                                       merge_dialects = T, 
-                                                                      method = "combine_random",
+                                                                      method = "singular_least_missing_data",
                                                                       replace_missing_language_level_ID = T, 
-                                                                      treat_question_mark_as_missing = T)
+                                                                      treat_question_mark_as_missing = T) %>% 
+  dplyr::select(-Language_ID) %>% 
+  dplyr::rename(Language_ID = Glottocode)
+
+#make Grambank ValueTable binary
+ValueTable_binary <- rgrambank::make_binary_ValueTable(ValueTable = ValueTable_dialect_reduced, 
+                                                       keep_multistate = F, keep_raw_binary = T)
 
 #prep for imputation
+
+source("../R/crop_missing_data.R")
 #crop such that features with lots of missing data and languages are removed
-ValueTable_cropped <- rgrambank::crop_missing_data(ValueTable = ValueTable_dialect_reduced, 
+ValueTable_cropped <- crop_missing_data(ValueTable = ValueTable_binary, 
                                         cut_off_parameters  = 0.75, 
                                         cut_off_languages = 0.75,
-                                        turn_question_mark_into_NA = T)
+                                        turn_question_mark_into_NA = T) %>% 
+  mutate(Value = str_replace_all(Value, "0", "0 - absent")) %>%
+  mutate(Value = str_replace_all(Value, "1", "1 - present")) %>% 
+  dplyr::select(Language_ID, Parameter_ID, Value) %>%
+  spread(key = Parameter_ID, value = Value, drop = FALSE) 
+  
+set.seed(1421)
 
-#make wide for imputation
-ValueTable_wide <- reshape2::dcast(data = ValueTable_cropped, 
-                                   formula = Glottocode ~ Parameter_ID, 
-                                   value.var = "Value")
 
 #imputation
-imputed_data <- ValueTable_wide %>%
-  column_to_rownames("Glottocode") %>% 
+imputed_data <- ValueTable_cropped %>%
+  column_to_rownames("Language_ID") %>% 
   as.matrix() %>%
   data.frame() %>%
   mutate_all(as.factor) %>% 
@@ -63,14 +70,26 @@ imputed_data <- ValueTable_wide %>%
 
 cat(paste0("The imputation OOB error is ", round(imputed_data$OOBerror, 2), ".\n"))
 
+
 # do Pricinpal Components Analysis on imputed dataset
 GB_PCA <- imputed_data$ximp %>% 
-  mutate_all(as.numeric) %>% 
+  as.data.frame() %>% 
+  rownames_to_column("Language_ID") %>% 
+  reshape2::melt(id = "Language_ID")  %>% 
+  mutate_all(as.character) %>% 
+  mutate(value =  str_replace_all(value, "0 - absent", "0")) %>%
+  mutate(value =  str_replace_all(value,  "1 - present", "1")) %>%
+  mutate(value = as.numeric(value)) %>%  
+  dplyr::select(Language_ID, Parameter_ID = variable, value) %>%
+  spread(key = Parameter_ID, value = value, drop = FALSE) %>% 
+  column_to_rownames("Language_ID") %>% 
   as.matrix() %>% 
   stats::prcomp(scale = T) 
 
 ###Map first 3 PCA components to RGB
 RGB_vec <- GB_PCA$x %>% 
+  as.data.frame() %>% 
+  dplyr::select(PC1, PC2, PC3) %>% 
   rgrambank::match_to_rgb(first_three = T)
 
 # there are records in the data now that don't have long/lat details in the LanguageTable, because they were dialects which were emrged. Therefore, we need long/lat data from glottolog
@@ -94,3 +113,10 @@ map <- basemap_list$basemap +
   geom_jitter(mapping = aes(x = Longitude, y = Latitude), color =  basemap_list$MapTable$RGB, size = 2)
 
 ggsave(plot = map, filename = "output/plots/PCA_RGB_map.png", width = 10, height = 10)
+
+library(SH.misc)
+
+SH.misc::basemap_EEZ(south = "down", colour_border_land = "white", colour_border_eez = "lightgray", padding = 0) +
+  geom_jitter(data = basemap_list$MapTable, mapping = aes(x = Longitude, y = Latitude), color =  basemap_list$MapTable$RGB, size = 2)
+
+ggsave(filename = "output/plots/PCA_RGB_map_eez.png", width = 10, height = 10)
