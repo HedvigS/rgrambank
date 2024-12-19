0dde2c4f5155b83d6e55352054d807cc8519e6f3
HedvigS <hedvig.skirgard@gmail.com>
2024-12-19 13:48:27 +0100

    Update example_make_theo_scores.R

--- example_scripts/example_make_theo_scores.R
+++ example_scripts/example_make_theo_scores.R
@@ -1,6 +1,3 @@
-
-Grambank_version <- 1
-
 #install.packages("devtools")
 library(devtools)
 
@@ -10,31 +7,14 @@ library(tidyverse)
 #install_github("SimonGreenhill/rcldf", dependencies = TRUE, ref = "v1.2.0")
 library(rcldf)
 
-#devtools::install_github("HedvigS/rgrambank", ref = "v1.0")
+#devtools::install_github("HedvigS/rgrambank", ref = "d61b62c738c706e125cfd8456b1c2732c9ceffbe")
 library(rgrambank)
 
-if(Grambank_version == 2){
-# This part of the script is written solely for people with access to Grambank v2, which is not public at the time of writing (2024-07-25).
-# Word_Order was missing from Grambank 2.0, this is being fixed:
-#https://github.com/glottobank/Grambank/issues/2782
-#in the meantime, grambank v2 will need to be complemented with the ParameterTable from v1.
-
-GB_rcldf_obj_v2 <- rcldf::cldf("../../../../Grambank 2.0/cldf/StructureDataset-metadata.json", load_bib = F)
-# fetching Grambank v1.0.3 from Zenodo using rcldf (requires internet)
-GB_rcldf_obj_v1 <- rcldf::cldf("https://zenodo.org/record/7844558/files/grambank/grambank-v1.0.3.zip", load_bib = F)
-LanguageTable <- GB_rcldf_obj_v2$tables$LanguageTable
-ValueTable <- GB_rcldf_obj_v2$tables$ValueTable
-ParameterTable <- GB_rcldf_obj_v1$tables$ParameterTable
-}
-
-
-if(Grambank_version == 1){
   # fetching Grambank v1.0.3 from Zenodo using rcldf (requires internet)
   GB_rcldf_obj_v1 <- rcldf::cldf("https://zenodo.org/record/7844558/files/grambank/grambank-v1.0.3.zip", load_bib = F)
   LanguageTable <- GB_rcldf_obj_v1$tables$LanguageTable
   ValueTable <- GB_rcldf_obj_v1$tables$ValueTable
   ParameterTable <- GB_rcldf_obj_v1$tables$ParameterTable
-  }
 
 ValueTable_binary <- rgrambank::make_binary_ValueTable(ValueTable = ValueTable, keep_multistate = FALSE, keep_raw_binary = TRUE)
 ParameterTable_binary <- rgrambank::make_binary_ParameterTable(ParameterTable = ParameterTable,
@@ -45,4 +25,25 @@ ValueTable_binary_reduced <- rgrambank::reduce_ValueTable_to_unique_glottocodes(
   dplyr::select(-Language_ID) %>% 
   dplyr::rename(Language_ID = Glottocode)
                                                                       
-theo_scores_table <- rgrambank::make_theo_scores(ValueTable = ValueTable_binary_reduced, ParameterTable = ParameterTable_binary)
+theo_scores_table <- rgrambank::make_theo_scores(ValueTable = ValueTable_binary_reduced, ParameterTable = ParameterTable_binary, Fusion_option = "count_one_and_half")
+
+theo_scores_table_excl_half <- rgrambank::make_theo_scores(ValueTable = ValueTable_binary_reduced, ParameterTable = ParameterTable_binary, Fusion_option = "count_one_only")
+
+theo_scores_table <- theo_scores_table %>% 
+  dplyr::select(Language_ID, `Fusion (one & half)` = Fusion)
+
+theo_scores_table_excl_half <- theo_scores_table_excl_half  %>% 
+  dplyr::select(Language_ID, `Fusion (one only)` = Fusion)
+
+theo_scores_table %>% 
+  full_join(theo_scores_table_excl_half, by = "Language_ID") %>%
+  ggplot(mapping = aes(x =`Fusion (one only)`, y = `Fusion (one & half)`)) +
+  geom_point( color = "#e3b839") +
+  theme_classic() +
+  ggpubr::stat_cor(method = "pearson", p.digits = 2, geom = "label", color = "blue",
+                   label.y.npc="top", label.x.npc = "left", alpha = 0.8) +
+  geom_smooth(method='lm', formula = 'y ~ x') 
+  
+  
+
+ggsave("output/Fusion_compare_options.png", width = 3, height = 3,  units = "in")
