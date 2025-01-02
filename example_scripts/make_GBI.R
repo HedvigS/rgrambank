a7f75f592ecb39ab638f048e5485457a2359095e
HedvigS <hedvig.skirgard@gmail.com>
2025-01-02 21:59:30 +0100

    Update make_GBI.R

--- example_scripts/make_GBI.R
+++ example_scripts/make_GBI.R
@@ -7,7 +7,7 @@ library(testthat)
 library(data.table)
 library(reshape2)
 
-ValueTable <- read.delim("../../../../grambank-v2.0rc2 2/cldf/values.csv", sep = ",") 
+#ValueTable <- read.delim("../../../../grambank-v2.0rc2 2/cldf/values.csv", sep = ",") 
 ValueTable <- read.delim("../../../grambank/grambank/cldf/values.csv", sep = ",") 
 
 load("../R/sysdata.rda")
