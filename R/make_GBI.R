dbe31de4f1d31ec5b5d4aee2dd8260f2e7f20cbb
HedvigS <hedvig.skirgard@gmail.com>
2025-01-02 19:22:37 +0100

    Update make_GBI.R

--- R/make_GBI.R
+++ R/make_GBI.R
@@ -9,6 +9,8 @@
 #' @import lsr
 #' @import data.table
 #' @author Anna Graff
+#' @export
+
 
 # this function serves to condition a feature on another -- note that the currently implemented function works for up to 5 desired states in the %in% case
 .implement_conditioning <- function(feature_to_be_conditioned, condition, equator){
