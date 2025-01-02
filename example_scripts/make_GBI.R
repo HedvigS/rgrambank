8f51941760bd68f3274b080a0bf9ba43bff5410e
HedvigS <hedvig.skirgard@gmail.com>
2025-01-02 21:58:57 +0100

    Update make_GBI.R

--- example_scripts/make_GBI.R
+++ example_scripts/make_GBI.R
@@ -1,6 +1,16 @@
+
+
+#remotes::install_github("Hedvigs/rgrambank")
+#library(rgrambank)
+library(tidyverse)
+library(testthat)
+library(data.table)
+library(reshape2)
+
 ValueTable <- read.delim("../../../../grambank-v2.0rc2 2/cldf/values.csv", sep = ",") 
+ValueTable <- read.delim("../../../grambank/grambank/cldf/values.csv", sep = ",") 
 
+load("../R/sysdata.rda")
+source("../R/make_GBI.R")
 
-remotes::install_github("Hedvigs/rgrambank")
-library(rgrambank)
-rgrambank::make_
+output <- make_GBI(ValueTable = ValueTable)
