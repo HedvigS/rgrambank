fc7b4d3ba4416a24fbc6f6b670e2f7d5b86fc230
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2025-01-28 15:02:51 +0100

    Update make_GBI.R

--- example_scripts/make_GBI.R
+++ example_scripts/make_GBI.R
@@ -12,12 +12,25 @@ GB_rcldf_obj <- rcldf::cldf("https://zenodo.org/record/7844558/files/grambank/gr
 
 Grambank_ValueTable <- GB_rcldf_obj$tables$ValueTable
 
-GBI <- rgrambank::make_GBI(ValueTable = ValueTable)
+GBI <- rgrambank::make_GBI(ValueTable = Grambank_ValueTable)
 
 # fetching Glottolog v5.0 from Zenodo using rcldf (requires internet)
 glottolog_rcldf_obj <- rcldf::cldf("https://zenodo.org/records/10804582/files/glottolog/glottolog-cldf-v5.0.zip", load_bib = F)
 
 Glottolog_ValueTable <- glottolog_rcldf_obj$tables$ValueTable 
 
-
 ###densify
+
+#checking that it doesn't run when both GBI and Grambank_ValueTable are defined
+densify_GB(GBI = GBI, Grambank_ValueTable = Grambank_ValueTable, Glottolog_ValueTable = Glottolog_ValueTable)
+
+#checking that it doesn't run when neither is defined
+densify_GB(Glottolog_ValueTable = Glottolog_ValueTable)
+
+#checking that it runs for Grambank_ValueTable
+GB_dense <- densify_GB(Grambank_ValueTable = Grambank_ValueTable, Glottolog_ValueTable = Glottolog_ValueTable)
+
+#checking that it runs for GBI
+GBI_dense <- densify_GB(GBI = GBI, Glottolog_ValueTable = Glottolog_ValueTable)
+
+
