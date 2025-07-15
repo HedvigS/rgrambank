7c44bfaa336d46e28a43c3d7b7c341e8232cfcb2
HedvigS <hedvig.skirgard@gmail.com>
2025-07-15 14:40:11 +0200

    add :: and imortFrom to basemap_pacific_center

--- R/basemap_pacific_center.R
+++ R/basemap_pacific_center.R
@@ -5,7 +5,17 @@
 #' @note DataTable is good to include because it will ensure that the records are matched and that the shifted Longitudes are used (necessary for making the plot pacific-centered).
 #' @return A list, first object is a ggplot2 layer of a Pacific-centered worldmap and the second object is a combination of LongLatTable and DataTable, with Longitude adjusted to match the map.
 #' @author Hedvig Skirgård
-#' @import ggplot2 
+#' @importFrom dplyr select
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom ggplot2 map_data
+#' @importFrom ggplot2 geom_polygon
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 element_blank
+#' @importFrom ggplot2 element_rect
+#' @importFrom ggplot2 coord_map
+#' @importFrom ggplot2 expand_limits
+#' @importFrom ggplot2 ggplot
 #' @export
 
 basemap_pacific_center <- function(LongLatTable = NULL, 
@@ -27,39 +37,39 @@ if(!all(DataTable$ID %in% LongLatTable$ID)){
   
   LongLatTable <- LongLatTable %>% 
     dplyr::select(ID, Longitude, Latitude) %>% 
-    mutate(Longitude = if_else(Longitude <= -25, Longitude + 360, Longitude)) #shifting the longlat of the dataframe to match the pacific centered map
+    dplyr::mutate(Longitude = if_else(Longitude <= -25, Longitude + 360, Longitude)) #shifting the longlat of the dataframe to match the pacific centered map
 
 Table <- DataTable %>% 
-  left_join(LongLatTable, by = "ID")
+  dplyr::left_join(LongLatTable, by = "ID")
 
 world <- ggplot2::map_data('world', wrap=c(-25,335), ylim=c(-56,80), margin=T)
 
 lakes <- ggplot2::map_data("lakes", wrap=c(-25,335), col="white", border="gray", ylim=c(-55,65), margin=T)
 
 #Basemap
-basemap <- ggplot(Table) +
-  geom_polygon(data=world, aes(x=long, y=lat, group=group),
+basemap <- ggplot2::ggplot(Table) +
+  ggplot2::geom_polygon(data=world, ggplot2::aes(x=long, y=lat, group=group),
                colour="gray87",
                fill="gray87", linewidth = 0.5) +
-  geom_polygon(data=lakes, aes(x=long, y=lat, group=group),
+  ggplot2::geom_polygon(data=lakes, ggplot2::aes(x=long, y=lat, group=group),
                colour="gray87",
                fill="white", linewidth = 0.3) +
-  theme(
-    legend.position = "none",
+  ggplot2::theme(
+    legend.position = "None",
     # all of these lines are just removing default things like grid lines, axes etc
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    axis.line = element_blank(),
-    panel.border = element_rect(colour = "black", fill = NA),
-    panel.background = element_rect(fill = "white"),
-    axis.text.x = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks = element_blank()
+    panel.grid.major = ggplot2::element_blank(),
+    panel.grid.minor = ggplot2::element_blank(),
+    axis.title.x = ggplot2::element_blank(),
+    axis.title.y = ggplot2::element_blank(),
+    axis.line = ggplot2::element_blank(),
+    panel.border = ggplot2::element_rect(colour = "black", fill = NA),
+    panel.background = ggplot2::element_rect(fill = "white"),
+    axis.text.x = ggplot2::element_blank(),
+    axis.text.y = ggplot2::element_blank(),
+    axis.ticks = ggplot2::element_blank()
   ) +
-  coord_map(projection = "vandergrinten", ylim=c(-55,73)) +
-  expand_limits(x = Table$Longitude, y = Table$Latitude)
+  ggplot2::coord_map(projection = "vandergrinten", ylim=c(-55,73)) +
+  ggplot2::expand_limits(x = Table$Longitude, y = Table$Latitude)
 
 
 list(basemap = basemap, MapTable = Table)
