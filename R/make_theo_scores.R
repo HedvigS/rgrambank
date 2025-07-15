bfe6ba4b045d5271ee1b5322cb3d7f93090db8c8
HedvigS <hedvig.skirgard@gmail.com>
2025-07-15 17:05:22 +0200

    lazy eval make_theo_scores

--- R/make_theo_scores.R
+++ R/make_theo_scores.R
@@ -32,11 +32,11 @@ if(!"GB203b" %in% ParameterTable$ID){
     #read in sheet with scores for whether a feature denotes fusion
     ParameterTable <- ParameterTable %>%
         dplyr::select(Parameter_ID = ID, Fusion = Boundness, Informativity, Locus_of_Marking, Word_Order, Gender_or_Noun_Class, Flexivity) %>%
-        dplyr::mutate(Fusion = as.numeric(Fusion)) %>%
-        dplyr::mutate(Gender_or_Noun_Class = as.numeric(Gender_or_Noun_Class)) %>%
-        dplyr::mutate(Flexivity = as.numeric(Flexivity)) %>%
-        dplyr::mutate(Locus_of_Marking = as.numeric(Locus_of_Marking)) %>%
-        dplyr::mutate(Word_Order = as.numeric(Word_Order))
+        dplyr::mutate(Fusion = as.numeric(.data[["Fusion"]])) %>%
+        dplyr::mutate(Gender_or_Noun_Class = as.numeric(.data[["Gender_or_Noun_Class"]])) %>%
+        dplyr::mutate(Flexivity = as.numeric(.data[["Flexivity"]])) %>%
+        dplyr::mutate(Locus_of_Marking = as.numeric(.data[["Locus_of_Marking"]])) %>%
+        dplyr::mutate(Word_Order = as.numeric(.data[["Word_Order"]]))
 
     if(Fusion_option == "count_one_and_half") {
       n_fusion_feats <- sum(ParameterTable$Fusion == 1, na.rm = T) +  
@@ -71,114 +71,135 @@ if(!"GB203b" %in% ParameterTable$ID){
     
     ValueTable <- ValueTable %>%
         dplyr::inner_join(ParameterTable , by = "Parameter_ID", relationship = "many-to-many") %>%
-        dplyr::filter(!is.na(Value)) %>%
+        dplyr::filter(!is.na(.data[["Value"]])) %>%
         dplyr::filter(Value != "?") %>%
-        dplyr::mutate(Value = as.numeric(Value))  #makes it possible to sum, mean etc
+        dplyr::mutate(Value = as.numeric(.data[["Value"]]))  #makes it possible to sum, mean etc
 
     #fusion counts
     
     if(Fusion_option == "count_one_and_half") {
     Fusion_df <- ValueTable %>%
-        dplyr::filter(!is.na(Fusion)) %>%
-        dplyr::filter(Fusion != 0) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::mutate(n = n()) %>%
-        dplyr::filter(n >= n_fusion_feats * missing_cut_off) %>%
-        dplyr::mutate(Value_weighted = ifelse(Fusion == 0.5 & Value == 1, 0.5, Value )) # replacing all instances of 1 for a feature that is weighted to 0.5 bound morph points to 0.5 
+        dplyr::filter(!is.na(.data[["Fusion"]])) %>%
+        dplyr::filter(.data[["Fusion"]] != 0) %>%
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::mutate(n = dplyr::n()) %>%
+        dplyr::filter(.data[["n"]] >= n_fusion_feats * missing_cut_off) %>%
+        dplyr::mutate(Value_weighted = ifelse(.data[["Fusion"]] == 0.5 & 
+                                                      .data[["Value"]] == 1, 
+                                                    yes = 0.5, 
+                                                    no = .data[["Value"]] )) # replacing all instances of 1 for a feature that is weighted to 0.5 bound morph points to 0.5 
       }
       
       if(Fusion_option == "count_one_only") {
         Fusion_df <- ValueTable %>%
-          dplyr::filter(!is.na(Fusion)) %>%
-          dplyr::filter(Fusion == 1) %>%
-          dplyr::group_by(Language_ID) %>%
-          dplyr::mutate(n = n()) %>%
-          dplyr::filter(n >= n_fusion_feats * missing_cut_off) %>% 
+          dplyr::filter(!is.na(.data[["Fusion"]])) %>%
+          dplyr::filter(.data[["Fusion"]] == 1) %>%
+          dplyr::group_by(.data[["Language_ID"]]) %>%
+          dplyr::mutate(n = dplyr::n()) %>%
+          dplyr::filter(.data[["n"]] >= n_fusion_feats * missing_cut_off) %>% 
           dplyr::rename(Value_weighted = Value)
         }
           
     if(Fusion_option == "count_zero_half_and_one") {
       Fusion_df <- ValueTable %>%
         dplyr::filter(!is.na(Fusion)) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::mutate(n = n()) %>%
-        dplyr::filter(n >= n_fusion_feats * missing_cut_off) %>% 
-        dplyr::mutate(Value_weighted = ifelse(Fusion == 0.5 & Value == 1, 0.5, Value )) %>%  # replacing all instances of 1 for a feature that is weighted to 0.5 bound morph points to 0.5 
-        dplyr::mutate(value_weighted = if_else(Fusion == 0, abs(value-1), value_weighted)) # reversing the values of the features that refer to free-standing markers 
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::mutate(n = dplyr::n()) %>%
+        dplyr::filter(.data[["n"]] >= n_fusion_feats * missing_cut_off) %>% 
+        dplyr::mutate(Value_weighted = ifelse(.data[["Fusion"]] == 0.5 & .data[["Value"]] == 1, 
+                                              yes = 0.5, no = .data[["Value"]])) %>%  # replacing all instances of 1 for a feature that is weighted to 0.5 bound morph points to 0.5 
+        dplyr::mutate(value_weighted = if_else(.data[["Fusion"]] == 0, 
+                                               yes = abs(.data[["value"]]-1), 
+                                               no = .data[["value_weighted"]])) # reversing the values of the features that refer to free-standing markers 
     }
     
-          
-      
-      
       lg_df_for_fusion_count <- Fusion_df %>% 
-        dplyr::group_by(Language_ID) %>%
-        dplyr::summarise(Fusion = mean(Value_weighted))
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::summarise(Fusion = mean(.data[["Value_weighted"]]))
 
     ##Flexivity scores
     lg_df_for_flex_count <- ValueTable  %>%
         dplyr::filter(!is.na(Flexivity)) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::mutate(n = n()) %>%
-        dplyr::filter(n >= n_flexivity_feats * missing_cut_off) %>%
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::mutate(n = dplyr::n()) %>%
+        dplyr::filter(.data[["n"]] >= n_flexivity_feats * missing_cut_off) %>%
         # reversing the Values of the features that have a score of 0
-        dplyr::mutate(Value_weighted = ifelse(Flexivity == 0, abs(Value-1), Value)) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::summarise(Flexivity = mean(Value_weighted), .groups = "drop")
+        dplyr::mutate(Value_weighted = ifelse(.data[["Flexivity"]] == 0, 
+                                              yes = abs(.data[["Value"]]-1), 
+                                              no = .data[["Value"]])) %>%
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::summarise(Flexivity = mean(.data[["Value_weighted"]]), .groups = "drop")
 
     ##`locus of marking`s
     lg_df_for_HM_DM_count <- ValueTable %>%
-        dplyr::filter(!is.na(Locus_of_Marking)) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::mutate(n = n()) %>%
-        dplyr::filter(n >= n_locus_marking_feats * missing_cut_off) %>%
+        dplyr::filter(!is.na(.data[["Locus_of_Marking"]])) %>%
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::mutate(n = dplyr::n()) %>%
+        dplyr::filter(.data[["n"]] >= n_locus_marking_feats * missing_cut_off) %>%
         # reversing the Values of the features that have a score of 0
-        dplyr::mutate(Value_weighted = ifelse(Locus_of_Marking == 0, abs(Value-1), Value)) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::summarise(Locus_of_Marking = mean(Value_weighted), .groups = "drop_last")
+        dplyr::mutate(Value_weighted = ifelse(.data[["Locus_of_Marking"]] == 0, 
+                                              yes = abs(.data[["Value"]]-1), 
+                                              no = .data[["Value"]])) %>%
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::summarise(Locus_of_Marking = mean(.data[["Value_weighted"]]), 
+                         .groups = "drop_last")
 
     ##Gender_or_Noun_Class scores
     lg_df_for_gender_nc_count <- ValueTable  %>%
-        dplyr::filter(!is.na(Gender_or_Noun_Class)) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::mutate(n = n()) %>%
-        dplyr::filter(n >= n_gender_NC_feats * missing_cut_off) %>%
+        dplyr::filter(!is.na(.data[["Gender_or_Noun_Class"]])) %>%
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::mutate(n = dplyr::n()) %>%
+        dplyr::filter(.data[["n"]] >= n_gender_NC_feats * missing_cut_off) %>%
         # reversing the Values of the features that have a score of 0
-        dplyr::mutate(Value_weighted = ifelse(Gender_or_Noun_Class == 0, abs(Value-1), Value)) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::summarise(Gender_or_Noun_Class = mean(Value_weighted), .groups = "drop_last")
+        dplyr::mutate(Value_weighted = ifelse(.data[["Gender_or_Noun_Class"]] == 0, 
+                                              yes = abs(.data[["Value"]]-1), 
+                                              no = .data[["Value"]])) %>%
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::summarise(Gender_or_Noun_Class = mean(.data[["Value_weighted"]]), 
+                         .groups = "drop_last")
 
      ##OV_VO scores
      lg_df_for_OV_VO_count <- ValueTable  %>%
-        dplyr::filter(!is.na(Word_Order)) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::mutate(n = n()) %>%
-        dplyr::filter(n >= n_word_order_feats * missing_cut_off) %>%
-        dplyr::mutate(Value_weighted = ifelse(Word_Order == 0, abs(Value-1), Value)) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::summarise(Word_Order = mean(Value_weighted), .groups = "drop_last")
+        dplyr::filter(!is.na(.data[["Word_Order"]])) %>%
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::mutate(n = dplyr::n()) %>%
+        dplyr::filter(.data[["n"]] >= n_word_order_feats * missing_cut_off) %>%
+        dplyr::mutate(Value_weighted = ifelse(.data[["Word_Order"]] == 0, 
+                                              yes = abs(.data[["Value"]]-1), 
+                                              no = .data[["Value"]])) %>%
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::summarise(Word_Order = mean(.data[["Value_weighted"]]), 
+                         .groups = "drop_last")
 
     ##informativity score
     lg_df_informativity_score <-  ValueTable  %>%
-        dplyr::filter(!is.na(Informativity)) %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::mutate(n = n()) %>%
-        dplyr::filter(n >= n_informativity_feats * missing_cut_off) %>%
+        dplyr::filter(!is.na(.data[["Informativity"]])) %>%
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::mutate(n = dplyr::n()) %>%
+        dplyr::filter(.data[["n"]] >= n_informativity_feats * missing_cut_off) %>%
         # reversing GB140 because 0 is the informative state
-        dplyr::mutate(Value = ifelse(Parameter_ID == "GB140", abs(Value-1), Value)) %>%
+        dplyr::mutate(Value = ifelse(.data[["Parameter_ID"]] == "GB140", 
+                                     yes = abs(.data[["Value"]]-1), 
+                                     no = .data[["Value"]])) %>%
         #grouping per language and per informativity category
-        dplyr::group_by(Language_ID, Informativity) %>%
+        dplyr::group_by(.data[["Language_ID"]], .data[["Informativity"]]) %>%
         # for each informativity category in each language,
         # .. how many are answered 1 ("yes")
         # how many of the Values per informativity category are missing
         dplyr::summarise(
-            sum_informativity = sum(Value), sum_na = sum(is.na(Value)), .groups = "drop_last" ) %>%
+            sum_informativity = sum(.data[["Value"]]), 
+            sum_na = sum(is.na(.data[["Value"]])), 
+            .groups = "drop_last" ) %>%
         # if there is at least one NA and the sum of Values for the entire category is 0, the
         # informativity score should be NA because there could be a 1 hiding under the NA Value.
-        dplyr::mutate(sum_informativity = ifelse(sum_na >= 1 & sum_informativity == 0, NA, sum_informativity)) %>%
-        dplyr::mutate(informativity_score = ifelse(sum_informativity >= 1, 1, sum_informativity)) %>%
+        dplyr::mutate(sum_informativity = ifelse(.data[["sum_na"]] >= 1 & .data[["sum_informativity"]] == 0, 
+                                                 yes = NA, no = .data[["sum_informativity"]])) %>%
+        dplyr::mutate(informativity_score = ifelse(.data[["sum_informativity"]] >= 1, 
+                                                   yes = 1, 
+                                                   no = .data[["sum_informativity"]])) %>%
         dplyr::ungroup() %>%
-        dplyr::group_by(Language_ID) %>%
-        dplyr::summarise(Informativity = mean(informativity_score), .groups = "drop_last")
+        dplyr::group_by(.data[["Language_ID"]]) %>%
+        dplyr::summarise(Informativity = mean(.data[["informativity_score"]]), .groups = "drop_last")
 
     lg_df_for_OV_VO_count %>%
         dplyr::full_join(lg_df_for_flex_count, by = "Language_ID") %>%
