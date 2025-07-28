f625271e309704eef68a57c45a3b8d78ee9c2f6f
HedvigS <hedvig.skirgard@gmail.com>
2025-07-28 21:35:29 +0200

    try.error -> try-error in varcov.spatial.3D

--- R/varcov.spatial.3D.R
+++ R/varcov.spatial.3D.R
@@ -176,11 +176,11 @@ varcov.spatial.3D <-
             if (func.inv == "eigen") {
                 varcov.eig <- try(eigen(varcov, symmetric = TRUE), silent=TRUE)
                 cov.logdeth <- try(sum(log(sqrt(varcov.eig$val))), silent=TRUE)
-                if (inherits(cov.logdeth, "try.error") || inherits(varcov.eig, "try-error")) {
+                if (inherits(cov.logdeth, "try-error") || inherits(varcov.eig, "try-error")) {
                     diag(varcov) <- 1.0001 * diag(varcov)
                     varcov.eig <- try(eigen(varcov, symmetric = TRUE), silent=TRUE)
                     cov.logdeth <- try(sum(log(sqrt(varcov.eig$val))), silent=TRUE)
-                    if (inherits(cov.logdeth, "try.error") || inherits(varcov.eig, "try-error")) {
+                    if (inherits(cov.logdeth, "try-error") || inherits(varcov.eig, "try-error")) {
                         return(list(crash.parms = c(tausq=tausq, sigmasq=sigmasq, phi=phi, kappa=kappa)))
                     }
                 }
