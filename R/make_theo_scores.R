f64dd3aaaf8c775a44842ae40de5916f39fb2995
HedvigS <hedvig.skirgard@gmail.com>
2026-07-07 22:49:17 +0200

    Update make_theo_scores.R

--- R/make_theo_scores.R
+++ R/make_theo_scores.R
@@ -15,7 +15,7 @@ make_theo_scores <- function(ValueTable,
 
   #we need the parameter and value table to be binarised, so we check if it is by looking for the existence of a known binarised feature
 if(!"GB203b" %in% ValueTable$Parameter_ID){
-        ValueTable <- ValueTable %>% make_binary_ValueTable()
+        ValueTable <- ValueTable %>% make_binary_ValueTable(keep_native_binary = TRUE, keep_multistate = FALSE,trim_to_only_native_binary = FALSE)
 }
 
 if(!"GB203b" %in% ParameterTable$ID){
