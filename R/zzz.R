2a069308f1c4ffc39de31ec4bcf6c0589060f7b8
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2026-07-27 12:22:41 +0200

    rm magrittr from zzz.r

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,6 @@
 # see http://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
 globalVariables(".")
 
-#'@importFrom magrittr "%>%"
 #'@importFrom rlang .data 
 #'
 NULL
