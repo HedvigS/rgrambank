f354b1c76fa7bd8e9382e2af468dc46c0c0f592e
HedvigS <hedvig.skirgard@gmail.com>
2026-08-03 21:49:37 +0200

    Update helper-fixtures.R

--- tests/testthat/helper-fixtures.R
+++ tests/testthat/helper-fixtures.R
@@ -67,6 +67,19 @@ Coders        = c("HS", "HS", "HS"),
 stringsAsFactors = FALSE
 )
 
+df_make_binary_multistate_mixed <- data.frame(
+  ID           = c("GB025-lang1", "GB130-lang1", "GB065-lang1", "GB065a-lang1", "GB065b-lang1"),
+  Language_ID  = c("lang1", "lang1", "lang1", "lang1", "lang1"),
+  Parameter_ID = c("GB025", "GB130", "GB065", "GB065a", "GB065b"),
+  Value        = c("1", "1", "3", "1", "0"),
+  Code_ID      = c("GB025-1", "GB130-1", "GB065-3", "GB065a-1", "GB065b-0"),
+  Comment      = c(NA, NA, NA, NA, NA), 
+  Source       = c("griff1", "griff1", "griff1", "griff1", "griff1"),  
+  Source_comment= c("griff1", "griff1", "griff1", "griff1", "griff1"),
+  Coders        = c("HS", "HS", "HS", "HS", "HS"),
+  stringsAsFactors = FALSE
+)
+
 
 
 
