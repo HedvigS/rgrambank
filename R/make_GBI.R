9adc762b274f8417ce4a782c35aeb659506d1d8a
HedvigS <hedvig.skirgard@gmail.com>
2026-08-03 17:13:59 +0200

    Update make_GBI.R

--- R/make_GBI.R
+++ R/make_GBI.R
@@ -24,7 +24,7 @@ make_GBI <- function(ValueTable = NULL,
   
   message("You're using the newer version of make_GBI")
   
-  .check_dups_ValueTable(ValueTable = ValueTable)
+  .check_dups_ValueTable(ValueTable = ValueTable, verbose = verbose)
 
 # Grambank v2 contains binarised features of the old multistate features from GB v1 (read more here: https://github.com/grambank/grambank/wiki/Binarised-features). The crossling-curated workflow currently calls for the mulistate features only, which is why the binarised (e.g. GB024a with values 0, 1 and ?) will be turned "back" into the multistate (e.g. GB024 with values 1, 2, 3 and ?).
 
