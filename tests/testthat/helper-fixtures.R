362baad50ba3af901b6ebf1e83923116d995bae8
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2026-08-04 11:48:14 +0200

    Update helper-fixtures.R

--- tests/testthat/helper-fixtures.R
+++ tests/testthat/helper-fixtures.R
@@ -11,6 +11,7 @@ df_check_dups <- data.frame(
   stringsAsFactors = FALSE
 )
 
+
 df_check_dups_clean <- data.frame(
   ID           = c("GB024-lang1", "GB065-lang1", "GB130-lang1"),
   Language_ID  = c("lang1", "lang1", "lang1"),
@@ -38,6 +39,20 @@ df_check_multistate_binary_clashes <- data.frame(
   Coders        = c("HS", "HS", "HS"),
   stringsAsFactors = FALSE
 )
+
+df_check_multistate_binary_clashes_one_missing <- data.frame(
+  ID           = c("GB025-lang1", "GB025a-lang1", "GB024b-lang1"),
+  Language_ID  = c("lang1", "lang1", "lang1"),
+  Parameter_ID = c("GB025", "GB025a", "GB024b"),
+  Value        = c("1", "1", "1"),
+  Code_ID      = c("GB025-1", "GB025a-1", "GB024b-1"),
+  Comment      = c(NA, NA, NA), 
+  Source       = c("griff1", "griff1", "griff1"),  
+  Source_comment= c("griff1", "griff1", "griff1"),
+  Coders        = c("HS", "HS", "HS"),
+  stringsAsFactors = FALSE
+)
+
 
 
 df_check_multistate_binary_clashes_clean <- data.frame(
@@ -80,10 +95,23 @@ df_make_binary_multistate_mixed <- data.frame(
   stringsAsFactors = FALSE
 )
 
+df_make_multistate_solo <- data.frame(
+  ID           = c("GB025-lang1", "GB130-lang1", "GB065-lang1", ),
+  Language_ID  = c("lang1", "lang1", "lang1", "lang1", "lang1"),
+  Parameter_ID = c("GB025", "GB130", "GB065", "GB065a", "GB065b"),
+  Value        = c("1", "1", "3", "1", "0"),
+  Code_ID      = c("GB025-1", "GB130-1", "GB065-3", "GB065a-1", "GB065b-0"),
+  Comment      = c(NA, NA, NA, NA, NA), 
+  Source       = c("griff1", "griff1", "griff1", "griff1", "griff1"),  
+  Source_comment= c("griff1", "griff1", "griff1", "griff1", "griff1"),
+  Coders        = c("HS", "HS", "HS", "HS", "HS"),
+  stringsAsFactors = FALSE
+)
 
 
 
-#df_make_multistate_solo
+
+
 
 #df_make_binary_multistate_mixed
 
