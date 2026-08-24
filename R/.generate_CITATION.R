c713fcd4e5d46ca444299901172fd4a87783abd8
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2026-08-24 15:45:25 +0200

    Update .generate_CITATION.R

--- R/.generate_CITATION.R
+++ R/.generate_CITATION.R
@@ -1,3 +1,5 @@
+library(glue)
+
 meta <- utils::packageDescription("rgrambank")
 
 # Get SHA
@@ -13,24 +15,37 @@ note <- if (!is.null(sha)) {
   paste0("R package dev version ", meta$Version)
 }
 
-# Build author string by collapsing all authors
+# Build authors
 authors <- eval(parse(text = meta$`Authors@R`))
+
+# Build each person() call separately
+author_lines <- sapply(authors, function(a) {
+  glue('    person("{a$given}", "{a$family}")')
+})
+
+# Collapse into a c() call
+author_block <- paste0(
+  "c(\n",
+  paste(author_lines, collapse = ",\n"),
+  "\n  )"
+)
+
+# Plain text version
 author_string <- paste(
   sapply(authors, function(a) paste(a$given, a$family)),
   collapse = ", "
 )
 
-# Write the static CITATION file with values filled in
-citation_content <- paste0(
-  'bibentry(
-  bibtype  = "Manual",
-  title    = "', meta$Title, '",
-  author   = person("', author_string, '"),
-  year     = "', format(Sys.Date(), "%Y"), '",
-  note     = "', note, '",
-  url      = "https://github.com/HedvigS/rgrambank",
-  textVersion = "', author_string, '. rgrambank: ', meta$Title, '. ', note, '. ', meta$URL, '"
-)'
+citation_content <- glue('
+bibentry(
+  bibtype      = "Manual",
+  title        = "rgrambank: {meta$Title}",
+  author       = {author_block},
+  year         = "{format(Sys.Date(), "%Y")}",
+  note         = "{note}",
+  url          = "https://github.com/HedvigS/rgrambank",
+  textVersion  = "{author_string}. rgrambank: {meta$Title}. {note}. https://github.com/HedvigS/rgrambank"
 )
+')
 
 writeLines(citation_content, con = "../inst/CITATION")
