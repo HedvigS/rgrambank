3b87fa3550ab0aff7fc23f597cfa7029d564f85d
HedvigS <hedvig.skirgard@gmail.com>
2026-09-11 15:30:30 +0200

    Update example_CultureFst.R

--- example_scripts/example_CultureFst.R
+++ example_scripts/example_CultureFst.R
@@ -5,6 +5,8 @@ library(rgrambank)
 # fetching Grambank v1.0.3 from Zenodo using rcldf (requires internet)
 GB_rcldf_obj <- rcldf::cldf("https://zenodo.org/record/7844558/files/grambank/grambank-v1.0.3.zip", load_bib = F)
 
+set.seed(32)
+
 PopTable <- GB_rcldf_obj$tables$LanguageTable |> 
   dplyr::group_by("ID") |> 
   dplyr::sample_n(size = 100, replace = FALSE) |> 
@@ -15,7 +17,7 @@ ValueTable_long <- GB_rcldf_obj$tables$ValueTable |>
   dplyr::filter(Language_ID %in% PopTable$ID) |> 
   dplyr::select(ID = Language_ID, Value, Parameter_ID) 
 
-cfx_object <- CultureFst(ValueTable_long = ValueTable_long, PopTable = PopTable, type = 0, bootstrap = T) 
+cfx_object <- CultureFst(ValueTable_long = ValueTable_long, PopTable = PopTable, type = 0, bootstrap = T, loci = NULL) 
 
 cfx_matrix <- cfx_object$mean.fst %>% as.matrix()
 cfx_matrix[upper.tri(x = cfx_matrix, diag = T)] <- NA
